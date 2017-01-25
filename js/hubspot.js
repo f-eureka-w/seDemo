5c744306bb20e25ce897e8bb931d0a3ce7d08129
Flora <feurekaw@gmail.com>
2017-01-24 21:33:16 -0500

    cookie time

--- js/hubspot.js
+++ js/hubspot.js
@@ -1,20 +1,46 @@
+// function submits on form submission 
+// uses serialize to spilt and organize data format that is readable 
+// ajax submission via POST method 
 
 $("form").submit(function( event ) {
   event.preventDefault();
   var data = $(this).serialize();
+  
   data = data.split("&");
   var firstName = data[0].split("=")[1];
   var lastName = data[1].split("=")[1];
   var email = data[2].split("=")[1].replace("%40", "@");
   var list = data[3].split("=")[1];
+  var cookies = document.cookies;
+  
+  cookies = cookies.split("; ");
+  var hsfirstvisit = cookies[0].split("=")[1];
+  var __hstc = cookies[1].split("=")[1];
+  var __hssrc = cookies[2].split("=")[1];
+  var hubspotutk = cookies[3].split("=")[1];
+
   $.ajax({
   	method:"POST",
   	url: "https://forms.hubspot.com/uploads/form/v2/1787193/6e2cf8f0-b777-4b7f-ba9f-5931af95f432",
-  	data: {firstName: firstName, lastName: lastName, email:email, what_analytics_do_you_need_:list}
+  	data: {
+  		firstName: firstName, 
+  		lastName: lastName, 
+  		email:email, 
+  		what_analytics_do_you_need_:list, 
+		hs_context:{
+			hsfirstvisit: hsfirstvisit,
+			__hstc: __hstc,
+			__hssrc:__hssrc,
+			hubspotutk:hubspotutk,
+		} }
 
   })
 });
 
+
+//"hsfirstvisit=https%3A%2F%2Ff-eureka-w.github.io%2FseDemo%2F||1485308822978; __hstc=5909356.de59346f29464ede746f1cbe83d63111.1485308822980.1485308822980.1485308822980.1; __hssrc=1; __hssc=5909356.12.1485308822981; hubspotutk=de59346f29464ede746f1cbe83d63111"
+
+// cookies to capture: hsfirstvisit, __hstc, __hssrc, hubspotutk 
 // //HID = 1787193
 // //Form GUID = 6e2cf8f0-b777-4b7f-ba9f-5931af95f432
 
